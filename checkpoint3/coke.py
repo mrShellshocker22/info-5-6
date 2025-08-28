daa8a0ad54a197e6774e5cea0c8858adbe266df8
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-08-28 20:12:43 +0000

    finished

--- checkpoint3/coke.py
+++ checkpoint3/coke.py
@@ -2,21 +2,26 @@ def main():
     name = input("Hello, Whats your name?: ")
     print(f"Awesome {name}, cokes are 50 cents you can insert coins of 25, 10 and 5 cents")
     coke_value = 50
-    loop(name,coke_value)
+    total_paid = 0
+    loop(name,coke_value,total_paid)
 
-def loop(name,coke_value):
-    while True:
+
+
+def loop(name,coke_value,total_paid):
+    while coke_value > 0:
+
         print(f"you're missing {coke_value}")
-        dinero = int(input("Please insert a coin: "))
+        pay = int(input("Please insert a coin: "))
 
-        if dinero != 25 or dinero != 10 or dinero != 5:
-            print("sorry we only accept cons of 25, 10 or 5 cents")
-            loop(name,coke_value)
-            
-        coke_value = coke_value - dinero
+        if pay == 25 or pay == 10 or pay == 5:
+            coke_value = coke_value - pay
+            total_paid = total_paid + pay
+        else:
+            print("not coin")
 
-        if coke_value <= 0:
-            print(f"Here you go {name}")
+        if total_paid >= 50:
+            print(f"Here you go {name}, aqui esta to coca")
+            print(f"heres your change {total_paid - 50}")
 
-        break
+
 main()
