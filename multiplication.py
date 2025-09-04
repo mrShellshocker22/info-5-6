331cb6f4bcbd06b0da93a2d610a854050e21de7c
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-09-04 20:17:12 +0000

    done

--- multiplication.py
+++ multiplication.py
@@ -2,6 +2,7 @@ def main():
     number = int(input("enter a positive number: "))
     if number > 0:
         multiplication(number)
+        tablas()
     else:
         print("please follow the instructions")
 
@@ -12,7 +13,24 @@ def multiplication(number):
 
     while i <= 10:
 
-            print(f"{number} X {i} = {number * i}")
+            print(f"{number} x {i} = {number * i}")
             i += 1
+
+def tablas():
+     
+    e = 1
+    a = 1
+     
+    print("hey do you know that")
+    
+    while e <= 10:
+     print(f"{e} x {a} = {e * a}")
+     
+     a += 1
+     if a > 10:
+        e += 1
+        a = 1
+        print("")
+
 
 main()
