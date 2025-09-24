7a2354bafac2b19e28ccc71acc7ae45d827639ac
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-09-24 20:09:56 +0000

    not done

---
+++ checkpoint 5/list-fun-practice.py
@@ -0,0 +1,16 @@
+def main():
+    list = [ ]
+    setp_one(list)
+
+
+def setp_one(list):
+    while elements != 0:
+        
+        elements = int(input("enter a number to add to the list"))
+        list.append(elements)
+
+        print(list)
+        list.sort()
+        print(list)
+
+main()
