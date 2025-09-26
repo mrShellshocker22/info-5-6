2b413adadd2881e4519b47b4886310e3a063761c
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-09-26 17:30:55 +0000

    ahhhhh

--- checkpoint 7/more-list.py
+++ checkpoint 7/more-list.py
@@ -22,8 +22,14 @@
 
 #Matrix
 matrix = [[1,2,3], [4,5,6], [7,8,9]]
-for row in matrix:
-    # for column in matrix:
-    #     column = row
-    #     print(column)
+length_row = len(matrix)
+lenght_coumn = len(matrix[0])
+
+for column_index in range(lenght_coumn):
+    new_matrix = []
+    for row_index in range(length_row):
+        new_matrix.append(matrix[row_index][column_index])
+    print(new_matrix)
+
+
 
