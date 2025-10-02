c7d4454067fb58ec67024532df0b001543227cd9
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-10-02 15:49:53 +0000

    uk

--- checkpoint 8/character-count.py
+++ checkpoint 8/character-count.py
@@ -13,4 +13,17 @@ character_counter(message, dictionary)
 
 print(f"your message have: {len(message)} characters")
 
+#alternative 1
+
+values_list = list(dictionary.values())
+keys_list = list(dictionary.keys())
+largest = max(values_list)
+print(f"the most repeated character is: {keys_list[largest]}, repeated {dictionary[keys_list[largest]]} times")
+
+
+#alternative 2
+
+largest_number2 = max(dictionary, key=dictionary.get)
+print(f"the most repeated character is: {largest_number2}, repeated {dictionary[largest_number2]} times")
+
 
