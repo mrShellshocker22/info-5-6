5814504f78387129b7bc1053f3bf9e6ea00dcfb0
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-10-07 16:15:33 +0000

    wr

--- checkpoint 9/scrabble2.py
+++ checkpoint 9/scrabble2.py
@@ -45,11 +45,11 @@ def score(word): # here we assign the points that each letter has
         10: ["Q", "Z"]
     }
     score = 0 # we set  our score to 0
-    for i in word:
+    for i in word: # we check the letters in the word we are gonna write and then it adds the points to the score
         for point, letters in score_chart.items():
             if i in letters:
                 score += point
-    return score
+    return score # prints the socre each time
 
 while True: 
     letters, list_letters = random_13_letters()
@@ -58,23 +58,23 @@ while True:
     wrong_characters = []
 
     valid = True
-    for i in word:
+    for i in word: 
         if i in list_letters:
-            list_letters.remove(i)
+            list_letters.remove(i) # if we already use the letter it takkes it out of the list
         else:
             wrong_characters.append(i)
             valid = False
 
-    if not valid:
+    if not valid: # if theres latter that is not in the list it tells you 
         print(f"Invalid input you do not have: {wrong_characters}")
         continue
 
-    with open("words.txt", "r") as file:
+    with open("words.txt", "r") as file: # it checks the document where we have all the words 
         words = {word.lower() for word in file.read().splitlines()}
 
-    if word.lower() not in words:
+    if word.lower() not in words: # if the word we put is not in the document it tells you so 
         print("Not a valid word")
         continue
 
-    if word.lower() in words:
+    if word.lower() in words: # if the word is in the document it tells you your score 
         print(f"your score is {score(word)}")
