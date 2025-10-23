871a45f2d5180e81486073158a1dce15defcfcbb
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-10-23 16:38:24 +0000

    finished

--- checkpoint 5/list-fun-practice.py
+++ checkpoint 5/list-fun-practice.py
@@ -1,17 +1,38 @@
 def main():
+    values = [ ]
+    setp_one(values)
+    length(values)
+    mean(values)
+    
+def setp_one(values):
 
-    setp_one()
+    while True:
+
+        elements = int(input("enter a number to add to the list (enter 0 to end program): "))
+
+        if elements == 0:
+            print("program is finished")
+            print(f"input: {values}")
+            values.sort()
+            print(f"sorted: {values}")
+            break
+
+        values.append(elements)
+
+        print(f"input: {values}")
+        values.sort()
+        print(f"sorted: {values}")
 
 
-def setp_one():
-    list = [ ]
-    while elements > 0:
-        
-        elements = int(input("enter a number to add to the list"))
-        list.append(elements)
+def length(values):
+    the_length = len(values)
+    print(f"The length of the list is: {the_length}")
 
-        print(list)
-        list.sort()
-        print(list)
+def mean(values):
+    the_mean = sum(values) / len(values)
+    print(f"The mean of the list is: {the_mean}")
+
+def range_of_list(values):
+    the_range = max(values) - min(values)
 
 main()
