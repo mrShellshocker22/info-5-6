572f232486ee1112782cc5870694c4ac18c8c89a
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-10-23 17:23:33 +0000

    ejs

---
+++ exceptions.py
@@ -0,0 +1,30 @@
+#Syntax error
+#print("Hello, world")
+
+# Value Error
+# try:
+#     x = int(input("Whats x?: "))
+#     print(f"x is equal to {x}")
+# except ValueError:
+#     print("x is not a number")
+
+
+# def spam(divided_by):
+#     try:
+#         return 42 / divided_by
+#     except ZeroDivisionError:
+#         print("what ever you want")
+# print(spam(2))
+# print(spam(12))
+# print(spam(0))
+# print(spam(1))
+
+# while True:
+#     try:
+#         x = int(input("Whats x?: "))
+    
+#     except ValueError:
+#         print("x is not a number") 
+#     else: 
+#         break
+# print(f"x is equal to {x}")
