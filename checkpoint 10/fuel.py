232c8a20c0bbd6031415bc98284e9bc575c13a1d
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-10-30 16:19:35 +0000

    fuel done

--- checkpoint 10/fuel.py
+++ checkpoint 10/fuel.py
@@ -1,17 +1,28 @@
 
-x = input("enter a fraction (like 1/2): ").split("/") 
+while True:
+    try:
+        x = input("enter a fraction (like 1/2): ").split("/") 
+        
+        num = int(x[0])
+        den = int(x[1])
+        percentage = round((num / den) * 100)
 
-num = int(x[0])
-den = int(x[1])
-percentage = round((num / den) * 100)
 
-try:
-    if percentage <= 1:
-        print("Your tank is: E")
-    if percentage >=  99 and percentage <= 100:
-        print("Your tank is: F")
-except ZeroDivisionError:
-    print("Input invalid, please try again")
-    
-
-print(percentage)
+        if percentage <= 1:
+            print("Your tank is: E")
+            break
+        if percentage >=  99 and percentage <= 100:
+            print("Your tank is: F")
+            break
+        else:
+            print(percentage)
+            break
+    except ZeroDivisionError:
+        print("Input invalid, please try again")
+        pass
+    except ValueError:
+        print("Input invalid, please try again")
+        pass
+    except IndexError:
+        print("Input invalid, please try again")
+        pass
