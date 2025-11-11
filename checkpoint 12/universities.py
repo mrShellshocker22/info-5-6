83b1e585133fe30bd2005110de099257990aa71c
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-11-11 16:16:18 +0000

    a

---
+++ checkpoint 12/universities.py
@@ -0,0 +1,5 @@
+universities_list = ["Eastern Arizona College", "BYU Pathway", "Tecmilenio", "Tec Monterrey CDJ", "UACJ"]
+universities = {
+    "Eastern Arizona College" : {"Majors" : , "Cost" : 7750, "Distance" : 428},
+    
+    }
