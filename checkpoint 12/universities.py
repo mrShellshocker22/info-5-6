faff71789ef2f618a53cae00da9c713d36287975
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-11-11 16:40:42 +0000

    ag

--- checkpoint 12/universities.py
+++ checkpoint 12/universities.py
@@ -1,5 +1,10 @@
 universities_list = ["Eastern Arizona College", "BYU Pathway", "Tecmilenio", "Tec Monterrey CDJ", "UACJ"]
 universities = {
     "Eastern Arizona College" : {"Majors" : , "Cost" : 7750, "Distance" : 428},
-    
+    "BYU Pathway" : {"Majors" : , "Cost" : , "Distance" : },
+    "Tec Milenio" : {"Majors" : , "Cost" : , "Distance" : },
+    "Tec Monterrey CDJ" : {"Majors" : , "Cost" : , "Distance" : },
+    "UACJ" : {"Majors" : , "Cost" : , "Distance" : },
     }
+
+print(universities[0])
