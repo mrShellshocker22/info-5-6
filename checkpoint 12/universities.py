c4781b18cf345df9d0237acb3c9f80aadad6258b
mrShellshocker22 <mrShellshocker22@gmail.com>
2025-11-13 18:05:24 +0000

    wtq3t

--- checkpoint 12/universities.py
+++ checkpoint 12/universities.py
@@ -1,10 +1,14 @@
-universities_list = ["Eastern Arizona College", "BYU Pathway", "Tecmilenio", "Tec Monterrey CDJ", "UACJ"]
+
 universities = {
-    "Eastern Arizona College" : {"Majors" : , "Cost" : 7750, "Distance" : 428},
-    "BYU Pathway" : {"Majors" : , "Cost" : , "Distance" : },
-    "Tec Milenio" : {"Majors" : , "Cost" : , "Distance" : },
-    "Tec Monterrey CDJ" : {"Majors" : , "Cost" : , "Distance" : },
-    "UACJ" : {"Majors" : , "Cost" : , "Distance" : },
+    "Eastern Arizona College" : {"Majors" : , "Cost" : 7750, "Distance" : 428, "Location" : "Thatcher"},
+    "BYU Pathway" : {"Majors" : , "Cost" : 18325, "Distance" : , "Location" : "Idaho" },
+    "Tec Milenio" : {"Majors" : , "Cost" : 14000, "Distance" : , "Location : "Chihuahua" },
+    "Tec Monterrey CDJ" : {"Majors" : , "Cost" : 135000, "Distance" : , "Location" : "Ciudad Juarez"},
+    "UACJ" : {"Majors" : , "Cost"  : 11520, "Distance" : , "Location" : "Ciudad Juarez"},
     }
 
-print(universities[0])
+try:
+    x = input("what university do you wanna know about?: " )
+    print(universities)
+except ValueError,IndexError:
+    print("something went off, please try again")
